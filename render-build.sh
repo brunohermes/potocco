18f7cda20be190ca768f2f559e5b3791bce6d25f
Bruno Hermes <48295386+brunohermes@users.noreply.github.com>
2025-08-01 11:58:17 -0300

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Instala o Chrome stable
+apt-get update
+apt-get install -y wget --no-install-recommends \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-stable --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+# Instala as dependências do Node
+npm install
